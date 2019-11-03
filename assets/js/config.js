a652e039bd9e6caaed32450db759b4196434c75b
Nistor Cristian <contact@nistorcristian.com>
2019-11-03 21:54:44 +0200

    Poveglia v3
    Signed-off-by: Nistor Cristian <contact@nistorcristian.com>

---
+++ assets/js/config.js
@@ -0,0 +1,13 @@
+/**
+* Config JS file for Poveglia
+*/
+
+var config = {
+    'share-selected-text': true,
+    'load-more': true,
+    'infinite-scroll': false,
+    'infinite-scroll-step': 1,
+    'disqus-shortname': 'hauntedthemes-demo',
+    'content-api-host': '',
+    'content-api-key': '',
+};
